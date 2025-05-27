3bdfb66615a039e05d2c322526fae9f6ead52a6c
panwonski <39884999+panwonski@users.noreply.github.com>
2025-05-27 20:16:33 +0200

    Added backendUrl to ReportTypeDefinition.ts

--- my-dynamic-app/src/app/report/configuration/ReportTypeDefinition.ts
+++ my-dynamic-app/src/app/report/configuration/ReportTypeDefinition.ts
@@ -1,10 +1,9 @@
 import {ReportGroupEnum} from "../enum/ReportGroupEnum";
 import {ReportNameEnum} from "../enum/ReportNameEnum";
-import { Injectable } from "@angular/core";
-import { AddressCriteriaComponent } from "../criteria/AddressCriteria/AddressCriteria.component";
-import { PriceCriteriaComponent } from "../criteria/PriceCriteria/PriceCriteria.component";
-import { DateCriteriaComponent } from "../criteria/DateCriteria/DateCriteria.component";
-
+import {Injectable} from "@angular/core";
+import {AddressCriteriaComponent} from "../criteria/AddressCriteria/AddressCriteria.component";
+import {PriceCriteriaComponent} from "../criteria/PriceCriteria/PriceCriteria.component";
+import {DateCriteriaComponent} from "../criteria/DateCriteria/DateCriteria.component";
 
 
 export class ReportItem {
@@ -12,13 +11,15 @@ export class ReportItem {
   group: ReportGroupEnum;
   title: string;
   tableHeader: string[];
+  backendUrl: string;
   criteriaList: any[]
 
-  constructor(name: ReportNameEnum, group: ReportGroupEnum, title: string, tableHeader: string[], criteriaComponentList: any[]) {
+  constructor(name: ReportNameEnum, group: ReportGroupEnum, title: string, tableHeader: string[], backendUrl: string, criteriaComponentList: any[]) {
     this.name = name;
     this.group = group;
     this.title = title;
     this.tableHeader = tableHeader;
+    this.backendUrl = backendUrl;
     this.criteriaList = criteriaComponentList;
   }
 }
@@ -31,6 +32,7 @@ export class ReportTypeDefinition {
       group: ReportGroupEnum.GROUP_A,
       title: "List of things with city",
       tableHeader: ["Name", "City", "Price"],
+      backendUrl: "http://localhost:8080/api/report/list-of-items-with-city",
       criteriaList: [
         {
           component: AddressCriteriaComponent,
@@ -71,6 +73,7 @@ export class ReportTypeDefinition {
       group: ReportGroupEnum.GROUP_B,
       title: "List of things without city",
       tableHeader: ["Name", "Price"],
+      backendUrl: "http://localhost:8080/api/report/list-of-items-without-city",
       criteriaList: [
         {
           component: PriceCriteriaComponent,
